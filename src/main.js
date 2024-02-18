927f779f0f8215a64368b23f96ecc45b3f63897b
IndraHardika <92752408+KZdra@users.noreply.github.com>
2024-02-18 15:43:07 +0700

    Upload Front End

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import VueRouter from 'vue-router'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App) // Create the Vue app instance
+
+// Use Vue Router
+app.use(VueRouter)
+
+// Use the router instance
+app.use(router)
+
+// Mount the app to the #app element in your HTML
+app.mount('#app')
+
+// Import Bootstrap JavaScript after creating the app instance
+import 'bootstrap/dist/js/bootstrap.js'
