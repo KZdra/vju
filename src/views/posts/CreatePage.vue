fd2896ff614f75e07898dad19d2cb37ed546aa31
IndraHardika <92752408+KZdra@users.noreply.github.com>
2024-02-19 18:38:25 +0700

    create function

--- src/views/posts/CreatePage.vue
+++ src/views/posts/CreatePage.vue
@@ -1,23 +1,109 @@
 <template>
-    <div class="container mt-custom">
-        <div class="row">
-            <div class="card border-0 rounded shadow-sm">
-                <div class="card-body">
-                    HALAMAN TAMBAH POSTS
-                </div>
+  <div class="container mt-custom">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card border-0 rounded shadow">
+          <div class="card-body">
+            <h4>TAMBAH POST</h4>
+            <hr />
+            <div v-if="validation.errors" class="mt-2 alert alert-danger">
+              <ul class="mt-0 mb-0">
+                <li v-for="(error, index) in validation.errors" :key="index">
+                  {{ `${error.param} : ${error.msg}` }}
+                </li>
+              </ul>
             </div>
+            <form @submit.prevent="store">
+              <div class="form-group">
+                <label for="nama" class="font-weight-bold mb-2">Nama</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="post.nama"
+                  placeholder="Masukkan Nama"
+                />
+              </div>
+              <div class="form-group mt-3">
+                <label for="content" class="font-weight-bold mb-2"
+                  >Jurusan</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="post.jurusan"
+                  placeholder="Masukkan Jurusan"
+                />
+              </div>
+              <div class="form-group mt-3">
+                <label for="content" class="font-weight-bold mb-2">Hobi</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="post.hobi"
+                  placeholder="Masukkan Hobi"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary mt-3">SIMPAN</button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-  name: 'CreatePage'
-  }
-  </script>
-  
-  <style>
-  
-  </style>
-  
-  
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+
+export default {
+  name: "CreatePage",
+  setup() {
+    //state posts
+    const post = reactive({
+      nama: "",
+      jurusan: "",
+      hobi: "",
+    });
+
+    //state validation
+    const validation = ref([]);
+
+    //vue router
+    const router = useRouter();
+
+    //method store
+    function store() {
+      let nama = post.nama;
+      let jurusan = post.jurusan;
+      let hobi = post.hobi;
+
+      axios
+        .post("http://localhost:3000/tambah", {
+          nama: nama,
+          jurusan: jurusan,
+          hobi: hobi,
+        })
+        .then(() => {
+          //redirect ke post index
+          router.push({
+            name: "posts.index",
+          });
+        })
+        .catch((error) => {
+          //assign state validation with error
+          validation.value = error.response.data;
+        });
+    }
+
+    //return
+    return {
+      post,
+      validation,
+      router,
+      store,
+    };
+  },
+};
+</script>
